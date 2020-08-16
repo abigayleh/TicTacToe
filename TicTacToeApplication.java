0a24e9c45e2de7a0c07143e3465bfd4c670f98f8
abigayleh <60492560+abigayleh@users.noreply.github.com>
2020-08-16 16:52:40 -0230

    Update TicTacToeApplication.java

--- TicTacToeApplication.java
+++ TicTacToeApplication.java
@@ -11,16 +11,8 @@ public class TicTacToeApplication {
 
 	public static void main(String[] args) {
 
-		while(true) {
-		Scanner start = new Scanner(System.in);
-		System.out.println("Would you like to play a game of Tic Tac Toe? [Yes or No]");
-		boolean game = start.hasNext("Yes");
-		
-			if(game) {
 			ConsoleRunner console = new ConsoleRunner();
 			console.mainLoop();
-			}
+			
 		}
-		
 	}
-}
